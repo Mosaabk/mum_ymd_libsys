10bb0227cc63123252a9d9b6aecd690e35628bb8
ahmed <ahmed@AYC-L001>
2019-03-06 18:07:14 +0200

    Ethan

--- src/com/ymd/libsys/Administrator.java
+++ src/com/ymd/libsys/Administrator.java
@@ -1,21 +1,80 @@
 package com.ymd.libsys;
 
+import com.alibaba.fastjson.JSON;
+
 public class Administrator {
 	
-	public void addNewBook() {
+	public static void addNewBook(Book book) {
+		String booksPath = "src/com/ymd/libsys/books";
+		String res = MyTool.readStringFromFile(booksPath);
+		if (res.equals("")) {
+			Books books = new Books();
+			books.getBooks().add(book);
+			String bookString = JSON.toJSONString(books);
+			MyTool.WriteStringToFile(bookString, booksPath);
+		} else {
+			String jsonDataString = MyTool.readStringFromFile(booksPath);
+			Books books = JSON.parseObject(jsonDataString, Books.class);
+			books.getBooks().add(book);
+			String bookString = JSON.toJSONString(books);
+			MyTool.WriteStringToFile(bookString, booksPath);
+		}
+	}
+	
+	public static void addCopyBook(BookCopy bookCopy) {
+		String bookCopiesPath = "src/com/ymd/libsys/bookCopies";
+		String res = MyTool.readStringFromFile(bookCopiesPath);
+		if (res.equals("")) {
+			BookCopies bookCopies = new BookCopies();
+			bookCopies.getBookCopies().add(bookCopy);
+			String bookCopiesString = JSON.toJSONString(bookCopies);
+			MyTool.WriteStringToFile(bookCopiesString, bookCopiesPath);
+		} else {
+			String jsonDataString = MyTool.readStringFromFile(bookCopiesPath);
+			BookCopies bookCopies = JSON.parseObject(jsonDataString, BookCopies.class);
+			bookCopies.getBookCopies().add(bookCopy);
+			String bookCopiesString = JSON.toJSONString(bookCopies);
+			MyTool.WriteStringToFile(bookCopiesString, bookCopiesPath);
+		}
+	}
+	
+	public static void addLibraryMember(Member member) {
+		String membersPath = "src/com/ymd/libsys/members";
+		String res = MyTool.readStringFromFile(membersPath);
+		if (res.equals("")) {
+			Members members = new Members();
+			member.setId(0);
+			members.getMembers().add(member);
+			String memberString = JSON.toJSONString(members);
+			MyTool.WriteStringToFile(memberString, membersPath);
+		} else {
+			String jsonDataString = MyTool.readStringFromFile(membersPath);
+			Members members = JSON.parseObject(jsonDataString, Members.class);
+			
+			int len = members.getMembers().size();
+			int lastId = members.getMembers().get(len - 1).getId();
+			lastId++;
+			member.setId(lastId);
+			
+			members.getMembers().add(member);
+			String memberString = JSON.toJSONString(members);
+			MyTool.WriteStringToFile(memberString, membersPath);
+		}
 		
 	}
 	
-	public void addCopyBook() {
-		
+	public static void EditLibraryMember(Member member) {
+		String membersPath = "src/com/ymd/libsys/members";
+		String jsonDataString = MyTool.readStringFromFile(membersPath);
+		Members members = JSON.parseObject(jsonDataString, Members.class);
+		members.getMembers().set(member.getId(), member);
+		String memberString = JSON.toJSONString(members);
+		MyTool.WriteStringToFile(memberString, membersPath);
 	}
 	
-	public void addLibraryMember() {
-		
-	}
-	
-	public void EditLibraryMember() {
-		
-	}
+	public static void main(String[] args) {
+      
+      
+  }
 
 }
