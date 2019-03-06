e1f9ae95d8048b48bfc572365abcee5a33ec0954
ahmed <ahmed@AYC-L001>
2019-03-06 19:05:07 +0200

    A

--- src/com/ymd/libsys/controller/LoginController.java
+++ src/com/ymd/libsys/controller/LoginController.java
@@ -3,10 +3,14 @@ package com.ymd.libsys.controller;
 import com.ymd.libsys.SystemUser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.scene.control.Alert.AlertType;
 
 
@@ -36,22 +40,41 @@ public class LoginController {
 
         SystemUser su = new SystemUser(s1, s2);
         int loginRes = su.login(s1, s2);
-
+        loginRes = 2;
         alert.setTitle("Login ... Test!");
-
+        int screen = -1;
         switch (loginRes){
             case 0:
                 System.out.println("fail");
                 break;
             case 1:
                 System.out.println("System User");
+                screen = 0;
                 break;
             case 2:
                 System.out.println("Admin");
+                screen = 1;
+                break;
             case 3:
                 System.out.println("Librarian");
+                screen = 2;
+                break;
+            case 4:
+                System.out.println("Super User");
+                screen = 3;
                 break;
 
+        }
+        if(screen == 1) {
+        	try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/ymd/libsys/view/Book.fxml"));
+                Parent root1 = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root1));  
+                stage.show();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
 
 
